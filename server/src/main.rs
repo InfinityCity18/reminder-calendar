eb87632b7a8ccdd88d1c3c08dcd1d54fb9c12e85
InfinityCity18 <kubawlas1@gmail.com>
2024-08-17 18:49:19 +0200

    added axum

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use axum::routing::{get, post};
+use axum::{Json, Router};
+use serde::{Deserialize, Serialize};
+
+const BIND_SOCK_ADDR: &str = "localhost:2137"; // change to take from env or arg !!!!!
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/", post(create_user));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind(BIND_SOCK_ADDR).await.unwrap();
+    axum::serve(listener, app).await?;
+    Ok(())
 }
+
+async fn send_reminders() -> {}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct RemindersData {}
